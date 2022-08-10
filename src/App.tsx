dc26ec84d74a1b47b227f4441ddb1a5a327ead76
Moult <vincent.barbot@weenove.fr>
2022-08-11 01:38:52 +0200

    update : animation with useAnimation not sure what to do always buggy on the side

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimation, Variants } from "framer-motion";
 import { NavBar } from "./components/NavBar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { GridSample } from "./components/GridSample";
@@ -8,23 +8,50 @@ import { UsersSample } from "./components/UsersSample";
 import { TableSample } from "./components/TableSample";
 import AnimatedPage from "./components/AnimatedPage";
 
+const exitDistance = 400;
+const pageMotionVariants: Variants = {
+  top: {
+    opacity: 0,
+    y: -exitDistance,
+  },
+  bottom: {
+    opacity: 0,
+    y: exitDistance,
+  },
+  normal: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
 function App() {
   const [selectedMenuId, setSelectedMenuID] = useState(1);
   const [lastSelectedMenuId, setLastSelectedMenuID] = useState(1);
-  const [fromTop, setFromTop] = useState(true);
+  const [fromTop, setFromTop] = useState(1);
+  const enterPageAnimation = useAnimation();
+  const exitPageAnimation = useAnimation();
 
   useEffect(() => {
-    setFromTop(selectedMenuId > lastSelectedMenuId);
-    console.log("cur" + selectedMenuId)
-    console.log("last" + lastSelectedMenuId)
+    (async () => {
+      if (selectedMenuId > lastSelectedMenuId) {
+        await enterPageAnimation.start("top");
+        await exitPageAnimation.start("bottom");
+        setFromTop(1);
+      } else {
+        await enterPageAnimation.start("bottom");
+        await exitPageAnimation.start("top");
+        setFromTop(-1);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedMenuId, lastSelectedMenuId]);
 
   const selectedMenuIdCallBack = (id: number) => {
     setLastSelectedMenuID(selectedMenuId);
     setSelectedMenuID(id);
   };
-
   const location = useLocation();
+
   return (
     <div className="w-screen h-screen">
       {/* <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 1 }}>
@@ -43,33 +70,49 @@ function App() {
               <Route
                 path="/"
                 element={
-                  <AnimatedPage fromTop={fromTop}>
+                  <motion.div
+                    initial={fromTop === 1 ? "top" : "bottom"}
+                    animate={enterPageAnimation}
+                    transition={{ duration: 2 }}
+                  >
                     <GridSample />
-                  </AnimatedPage>
+                  </motion.div>
                 }
               />
               <Route
                 path="/data"
                 element={
-                  <AnimatedPage fromTop={fromTop}>
+                  <motion.div
+                    initial={fromTop === 1 ? "top" : "bottom"}
+                    animate={enterPageAnimation}
+                    transition={{ duration: 2 }}
+                  >
                     <TableSample />
-                  </AnimatedPage>
+                  </motion.div>
                 }
               />
               <Route
                 path="/users"
                 element={
-                  <AnimatedPage fromTop={fromTop}>
+                  <motion.div
+                    initial={fromTop === 1 ? "top" : "bottom"}
+                    animate={enterPageAnimation}
+                    transition={{ duration: 2 }}
+                  >
                     <UsersSample />
-                  </AnimatedPage>
+                  </motion.div>
                 }
               />
               <Route
                 path="/admin"
                 element={
-                  <AnimatedPage fromTop={fromTop}>
+                  <motion.div
+                    initial={fromTop === 1 ? "top" : "bottom"}
+                    animate={enterPageAnimation}
+                    transition={{ duration: 2 }}
+                  >
                     <FormsSample />
-                  </AnimatedPage>
+                  </motion.div>
                 }
               />
             </Routes>
